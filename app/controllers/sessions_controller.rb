fa7fe8158f1289fc55f4f06d0154dc0f15dd037a
sumancrest0001 <suman.crest0001@gmail.com>
2019-11-29 03:07:16 +0545

    completed all before post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
-  def new    
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      sign_in @user
+      set_current_user @user
+    else
+       flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out if signed_in?
+    redirect_to root_url
   end
 end
