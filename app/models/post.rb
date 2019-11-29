039896a1eb59551e53f074939d417e5456f254fc
sumancrest0001 <suman.crest0001@gmail.com>
2019-11-30 02:14:06 +0545

    just need to add error message

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :body, presence: true, length: { maximum: 250 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   attr_accessor :remember_digest
-  
+  has_many :posts
   before_create :create_remember_token
   before_save { email.downcase! }
-
   validates :name, presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
