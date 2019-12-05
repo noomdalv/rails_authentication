931a487b25313a8520a6720b1adc0b30885087c4
sumancrest0001 <suman.crest0001@gmail.com>
2019-12-05 23:28:21 +0545

    fixed all the errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,22 +10,12 @@ module ApplicationHelper
     end
   end
 
-  def sign_in(user)
-    session[:user_id] = user.id
+  def current_user=(user)
+    @current_user = user
   end
 
-  attr_writer :current_user
-
   def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user& authenticated?(cookies[:remember_token])
-        sign_in user
-        @current_user = user
-      end
-    end
+    @current_user ||= User.find_by(id: cookies.signed[:user_id])
   end
 
   def signed_in?
@@ -37,6 +27,7 @@ module ApplicationHelper
   end
 
   def remember(user)
+    session[:user_id] = user.id
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_digest
   end
